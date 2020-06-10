b82c77c14617d9f22ee75f46c2da86038ca6bd21
ascooper57 <cooper@pobox.com>
2020-06-10 12:23:08 -0700

    test harness works!

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,8 +1,6 @@
 import logging
 
-import psycopg2
 import pytest
-
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -13,13 +11,8 @@ def fixture_directory():
     import os
     return os.path.join(os.path.dirname(__file__), "fixtures")
 
+
 @pytest.fixture(scope='function')
-def flask_app_client():
-    from api import create_app
-    app = create_app('test')
-    app_context = app.app_context()
-    app_context.push()
-    app.testing = True
-    app.config['WTF_CSRF_ENABLED'] = False
-    client = app.test_client(use_cookies=True)
-    return client
+def app():
+    from api.routes import app
+    return app
--- backend/tests/units/routes_test.py
+++ backend/tests/units/routes_test.py
@@ -1,9 +1,21 @@
 # -*- coding: utf-8 -*-
 
+import json
+import pytest
 
-def test_hello(flask_app_client):
-    client = flask_app_client
+
+@pytest.mark.options(debug=False)
+def test_app(app):
+  assert not app.debug, 'Ensure the app not in debug mode'
+  pass
+
+
+@pytest.mark.options(debug=True)
+def test_hello(client):
     response = client.get("/")
     assert 200 == response.status_code
     assert '200 OK' == response.status
     assert 'utf-8' == response.charset
+    data = json.loads(response.data)
+    assert data['hello'] == 'world'
+
