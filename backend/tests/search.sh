b3e4f17894a9ce1989b38649c4380d7f6cd55962
ascooper57 <cooper@pobox.com>
2020-06-25 10:55:18 -0700

    Added "sort" parameter

--- backend/tests/search.sh
+++ backend/tests/search.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -H "Content-Type: application/json" -X POST -d '{"allOfTheseWords": "IUU", "orTerms": null, "exactTerms": null, "fileType": null, "language": "any", "excludeTerms": null, "lowRange": null, "highRange": null, "country": "any", "start": 1, "siteSearch": null, "termsAppearing": null}' http://localhost:5000/search
+curl -H "Content-Type: application/json" -X POST -d '{"allOfTheseWords": "IUU", "orTerms": null, "exactTerms": null, "fileType": null, "language": "any", "excludeTerms": null, "lowRange": null, "highRange": null, "country": "any", "start": 1, "siteSearch": null, "termsAppearing": null, "sort": null}' http://localhost:5000/search
