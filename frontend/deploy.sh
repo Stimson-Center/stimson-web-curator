d4ccce86e0cd722a547ff2c808be664b5311c956
ascooper57 <cooper@pobox.com>
2020-07-09 09:30:30 -0700

    Google Cloud is shutting down uwsgi / nginx service when run as root
    compiled with version: 8.3.0 on 09 May 2020 21:28:19
    os: Linux-4.19.112+ #1 SMP Sat Jun 13 11:04:33 PDT 2020
    nodename: 6bef4b0115de
    machine: x86_64
    clock source: unix
    pcre jit disabled
    detected number of CPU cores: 1
    current working directory: /app
    detected binary path: /usr/local/bin/uwsgi
    your memory page size is 4096 bytes
    detected max file descriptor number: 1048576
    lock engine: pthread robust mutexes
    thunder lock: disabled (you can enable it with --thunder-lock)
    uwsgi socket 0 bound to UNIX address /tmp/uwsgi.sock fd 3
    uWSGI running as root, you can use --uid/--gid/--chroot options
    *** WARNING: you are running uWSGI as root !!! (use the --uid flag) ***
    Python version: 3.8.2 (default, Apr 23 2020, 14:22:33)  [GCC 8.3.0]
    *** Python threads support is disabled. You can enable it with --enable-threads ***
    Python main interpreter initialized at 0x55b537ac84b0
    uWSGI running as root, you can use --uid/--gid/--chroot options
    *** WARNING: you are running uWSGI as root !!! (use the --uid flag) ***
    your server socket listen backlog is limited to 100 connections
    your mercy for graceful operations on workers is 60 seconds
    mapped 1239640 bytes (1210 KB) for 16 cores
    *** Operational MODE: preforking ***
    46:49,724 INFO success: nginx entered RUNNING state, process has stayed up for > than 1 seconds (startsecs)
    46:49,724 INFO success: uwsgi entered RUNNING state, process has stayed up for > than 1 seconds (startsecs)
    WSGI app 0 (mountpoint='') ready in 6 seconds on interpreter 0x55b537ac84b0 pid: 10 (default app)
    uWSGI running as root, you can use --uid/--gid/--chroot options
    *** WARNING: you are running uWSGI as root !!! (use the --uid flag) ***
    *** uWSGI is running in multiple interpreter mode ***
    spawned uWSGI master process (pid: 10)
    spawned uWSGI worker 1 (pid: 13, cores: 1)
    spawned uWSGI worker 2 (pid: 14, cores: 1)
    running "unix_signal:15 gracefully_kill_them_all" (master-start)...
    53:29,390 WARN received SIGTERM indicating exit request
    53:30,133 INFO waiting for nginx, uwsgi to die
    Thu Jul  9 15:53:30 2020 - graceful shutdown triggered...
    Gracefully killing worker 2 (pid: 14)...
    Gracefully killing worker 1 (pid: 13)...
    53:33,188 INFO waiting for nginx, uwsgi to die
    worker 1 buried after 5 seconds
    worker 2 buried after 5 seconds
    goodbye to uWSGI.
    53:35,260 INFO stopped: uwsgi (exit status 0)
    53:35,520 INFO stopped: nginx (exit status 0)

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -4,7 +4,8 @@ gcloud -v
 gcloud components update
 if [ -d "build" ]; then rm -rf "build"; fi
 npm run build
-gcloud app deploy --project=stimson-web-curator-ui  --verbosity=debug
+gcloud config set project stimson-web-curator-ui
+gcloud app deploy --project=stimson-web-curator-ui --stop-previous-version --promote --verbosity=debug
 gcloud app describe --project=stimson-web-curator-ui
-gcloud app logs tail -s stimson-web-curator-ui
+gcloud app logs tail -s default
 
