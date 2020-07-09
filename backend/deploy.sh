04b05f8ba94a362ffc95a43a11cddb26b6bbb0fc
ascooper57 <cooper@pobox.com>
2020-07-09 14:25:31 -0700

    Update deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -4,6 +4,8 @@ gcloud -v
 gcloud components install app-engine-python
 gcloud components update
 gcloud config set project stimson-web-curator-api
-gcloud app deploy --project=stimson-web-curator-api --stop-previous-version --promote --verbosity=debug
+# gcloud app deploy --project=stimson-web-curator-api --stop-previous-version --promote --verbosity=debug
+gcloud app deploy --project=stimson-web-curator-api --stop-previous-version --promote
 gcloud app describe --project=stimson-web-curator-api
+gcloud app browse
 gcloud app logs tail -s default
