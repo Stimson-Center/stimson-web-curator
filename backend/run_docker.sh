fe4780b76dc3b19487eef1972cd9cc6533615509
ascooper57 <cooper@pobox.com>
2020-07-10 20:04:57 -0700

    internally listen to port 8080, externally port 80

--- backend/run_docker.sh
+++ backend/run_docker.sh
@@ -3,4 +3,4 @@
 docker system prune --force
 rm -rf backend/.venv
 docker build -t stimson-web-curator-api . 
-docker run -p 8080:8080 -it stimson-web-curator-api
+docker run -p 80:80 -it stimson-web-curator-api
