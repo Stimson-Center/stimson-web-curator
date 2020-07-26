b08ac45a9d0da56ce88956f60c642b9d9d1ef5bf
ascooper57 <cooper@pobox.com>
2020-07-26 06:45:16 -0700

    Refactored for new article translation API

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -16,32 +16,44 @@ export function Article({...props}) {
   const url = props.url;
   const language = props.language;
   const translate = props.translate;
+  const final_step = translate ? "TRANSLATED" : "NLPED";
   // https://medium.com/swlh/how-to-use-useeffect-on-server-side-654932c51b13
   useEffect(() => {
     const fetchData = async () => {
-      // console.log("In Article fetchData url=" + url);
+      // console.log("In Article fetchData workflow=" + JSON.stringify(article.workflow));
       if (url !== null && article.workflow.length === 0) {
-        const scraperApiUrl = getScraperBaseUrl().concat(encodeURI('/article?url=' + url + '&language=' + language + "&translate=" + translate));
+        const scraperApiUrl = getScraperBaseUrl().concat(encodeURI('/article?url=' + url + '&language=' + language));
         let response1 = await axios.get(scraperApiUrl);
         if (!isEmpty(response1) && !isEmpty(response1.data)) {
           // console.log("Article1: data=" + JSON.stringify(response1.data.workflow, null, 2));
           setArticle(response1.data);
         }
-      }
-      if (!article.workflow.includes("NLPED")) {
-        await sleep(1000);
-        const scraperApiUrl = getScraperBaseUrl().concat('/article');
-        axios.post(scraperApiUrl, article)
-          .then(response2 => {
-            // console.log("Article2: data=" + JSON.stringify(response2.data.workflow, null, 2));
-            setArticle(response2.data);
-          })
-          .catch(error => {
-            console.log(error);
-          })
-      }
-      if (article.workflow.includes("NLPED")) {
-        notify(props.onProgress, {article: article});
+      } else if (article.url) {
+        if (!article.workflow.includes("NLPED")) {
+          await sleep(1000);
+          const scraperApiUrl = getScraperBaseUrl().concat('/article');
+          axios.post(scraperApiUrl, article)
+            .then(response2 => {
+              // console.log("Article2: data=" + JSON.stringify(response2.data.workflow, null, 2));
+              setArticle(response2.data);
+            })
+            .catch(error => {
+              console.log(error);
+            })
+        } else if (translate && !article.workflow.includes("TRANSLATED")) {
+            const scraperApiUrl = getScraperBaseUrl().concat('/article/translate');
+            axios.post(scraperApiUrl, article)
+              .then(response3 => {
+                // console.log("Article3: data=" + JSON.stringify(response3.data.workflow, null, 2));
+                setArticle(response3.data);
+              })
+              .catch(error => {
+                console.log(error);
+              })
+        }
+        if (article.workflow.includes(final_step)) {
+          notify(props.onProgress, {article: article});
+        }
       }
     }
     // noinspection JSIgnoredPromiseFromCall
@@ -51,18 +63,22 @@ export function Article({...props}) {
   let progressText = 'Initialization should be brief';
   let progressPercent = 0;
   if (article.workflow.includes('INIT')) {
-    progressPercent = 25
+    progressPercent = 20
     progressText = 'Downloading can take a minute';
   }
   if (article.workflow.includes('DOWNLOADED')) {
-    progressPercent = 50
+    progressPercent = 40
     progressText = 'Parsing can take a minute';
   }
   if (article.workflow.includes('PARSED')) {
-    progressPercent = 75
+    progressPercent = 60
     progressText = 'Natural Language Processing can take a minute';
   }
   if (article.workflow.includes('NLPED')) {
+    progressPercent = 80
+    progressText = 'Translation.';
+  }
+  if (article.workflow.includes(final_step)) {
     progressPercent = 100
     progressText = 'Done.';
   }
