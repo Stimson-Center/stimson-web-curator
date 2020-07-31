b67496403e55dbfe8e456d773048761714fe3521
ascooper57 <cooper@pobox.com>
2020-07-31 11:25:59 -0700

    Update Article.jsx

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 // reactstrap components
 import {Progress} from "reactstrap";
 import axios from "axios";
-import {getScraperBaseUrl, isEmpty, sleep} from '../../Utils';
+import {getScraperBaseUrl, sleep} from '../../Utils';
 
 function notify(handler, args) {
   handler && handler.apply(null, [].concat(args));
